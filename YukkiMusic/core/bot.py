1644cc40d8047b5de4346f041e1cf671e11736ae
zergiogithub <138329164+zergiorvdr@users.noreply.github.com>
2023-08-18 11:57:08 +0700

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -21,7 +21,7 @@ class YukkiBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "YukkiMusicBot",
+            "ZerMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
